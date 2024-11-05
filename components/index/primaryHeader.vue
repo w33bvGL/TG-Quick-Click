a1f47f6132d1090dced56abfc38f9e5597d016e3
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-11-05 12:38:42 +0400

    add primary header

--- components/index/primaryHeader.vue
+++ components/index/primaryHeader.vue
@@ -4,33 +4,50 @@
 
 <template>
     <section class="primary-header" id="primary-header">
-        <div class="vlada-container md:mt-20 md:mb-40">
-            <div class="bg-vlada-color-7 rounded-2xl md:px-7 min-h-[679px] max-h-[679px] flex justify-between">
-                <div class="relative w-1/2">
-                    <NuxtImg src="/images/primary-header-clicker-app-image.svg" loading="lazy"
-                             class="absolute md:left-28 -top-10 select-none pointer-events-none"/>
+        <div class="vlada-container md:mt-20 2xl:mb-28">
+            <div class="bg-vlada-color-7 rounded-2xl md:px-7 2xl:min-h-[679px] max-h-[679px] flex justify-between">
+                <div class="relative w-1/2 hidden 2xl:block">
+                    <NuxtImg src="/images/primary-header-clicker-app-image.svg"
+                             class="absolute md:left-28 h-full scale-110 select-none pointer-events-none" alt="Изображение заголовка кликер-приложения"/>
                 </div>
-                <div class="relative w-1/2">
+                <div class="relative w-full 2xl:w-1/2">
                     <button
-                        class="bg-vlada-color-4 absolute right-0 -top-8 rounded-full border-4 border-white py-6 px-9 text-white text-2xl uppercase italic font-bold">
+                        class="hidden  2xl:block bg-vlada-color-4 absolute right-0 -top-8 rounded-full border-4 border-white py-6 px-9 text-white text-2xl uppercase italic font-bold">
                         Получить консультацию бесплатно
                     </button>
-                    <div class="relative md:mt-24">
-                        <h1 class="text-5xl font-medium">Готовый <span class="uppercase font-extrabold text-8xl italic">web app</span><br><span
-                            class="md:pl-20 font-extrabold text-8xl italic">Кликер</span><br>За 10 минут</h1>
-                        <div class="mt-5 px-28">
-                            <h2 class="text-vlada-color-9 text-3xl font-bold">Получите готовую копию кликера, магазина
+                    <div class="relative 2xl:mt-24">
+                        <div class="flex justify-center 2xl:justify-start w-full pt-5">
+                            <h1 class="text-base md:text-5xl font-medium ">Готовый <span
+                                class="uppercase font-extrabold text-6xl md:text-8xl italic">web app</span><br>
+                                <span class="md:pl-20 font-extrabold text-6xl md:text-8xl italic">Кликер</span>
+                                <br>За 10 минут
+                            </h1>
+                        </div>
+                        <div class="mt-5 px-4 pb-5 2xl:pb-0 md:px-28 w-full text-center 2xl:text-left">
+                            <h2 class="text-vlada-color-9 text-xl md:text-3xl font-bold md:line-clamp-2">Получите готовую копию кликера,
+                                магазина
                                 или пирамиды за 10 минут.</h2>
-                            <h2 class="text-vlada-color-9 text-3xl mt-6 font-bold">Мы запустим для вас mini-app, а так
+                            <h2 class="text-vlada-color-9 text-xl md:text-3xl mt-2 md:mt-6 font-bold md:line-clamp-2">Мы запустим для
+                                вас mini-app, а так
                                 же модифицируем под ваши нужды.</h2>
                         </div>
-                        <div class="w-full flex items-end justify-end mt-14">
+                        <div class="w-full hidden items-end justify-end mt-14 2xl:flex">
                             <button class="text-3xl font-bold text-white bg-vlada-color-8 py-4 px-48 rounded-full">
                                 Получить
                             </button>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="flex 2xl:hidden py-3 mb-6 md:mb-10 flex-col gap-3">
+                <button
+                    class="text-xl md:text-2xl italic font-bold text-white bg-vlada-color-8 py-4 rounded-2xl w-full">
+                    Получить
+                </button>
+                <button
+                    class="text-xl md:text-2xl italic font-bold text-white bg-vlada-color-3 py-4 rounded-2xl w-full">
+                    Бесплатная консультация
+                </button>
             </div>
         </div>
     </section>
