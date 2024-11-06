54869e8c3bcf79808446fd2c1efd246749d38b9d
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-11-06 07:07:19 +0400

    Update weOffer.vue

--- components/index/weOffer.vue
+++ components/index/weOffer.vue
@@ -5,56 +5,85 @@
 <template>
     <section class="we-offer" id="we-offer">
         <div class="vlada-container 2xl:px-32 md:mt-20 2xl:mb-28">
-            <div class="flex justify-between items-center">
-                <div class="relative 2xl:w-1/2">
-                    <h3 class="text-white text-center md:text-black text-2xl md:text-5xl">Мы предлагаем:</h3>
+            <div class="flex justify-between items-center flex-col xl:flex-row xl:gap-4 mb-9">
+                <div class="relative 2xl:w-1/2 mb-3 xl:mb-0">
+                    <h3 class="text-white text-center md:text-black text-3xl md:text-5xl">Мы предлагаем:</h3>
                     <NuxtImg src="/images/we-offer-clicker-app-image.svg"
-                             class="mt-10"
+                             class="mt-10 hidden xl:block w-full"
                              alt="Изображение заголовка кликер-приложения"/>
                 </div>
                 <div class="relative 2xl:w-1/2">
-                    <hr class="w-full border-t-2 border-black"/>
-                    <div class="flex gap-5 py-5">
-                        <span class="text-3xl font-bold">01</span>
-                        <div>
-                            <h4 class="text-3xl font-bold">Покупка готовых Web-app</h4>
-                            <p class="mt-3 text-2xl">Получите готовый кликер, интернет-магазин, P2E игру и<br> начните
-                                путь в мире
-                                mini-apps</p>
-                            <button class="px-20 mt-5 border-2 border-black text-2xl py-0.5 flex">приобрести</button>
-                        </div>
-                    </div>
-                    <hr class="w-full border-t-2 border-black"/>
-                    <div class="flex gap-5 py-5">
-                        <span class="text-3xl font-bold">02</span>
-                        <div>
-                            <h4 class="text-3xl font-bold">Обновление дизайна существующего кликера</h4>
-                            <p class="mt-3 text-2xl">Модифицируем дизайн для вас под любые нужды или изменим кликер под
-                                ваш дизайн</p>
-                            <button class="px-20 mt-5 border-2 border-black text-2xl py-0.5 flex">приобрести</button>
-                        </div>
-                    </div>
-                    <hr class="w-full border-t-2 border-black"/>
-                    <div class="flex gap-5 py-5">
-                        <span class="text-3xl font-bold">03</span>
-                        <div>
-                            <h4 class="text-3xl font-bold">Разработка Web-app под ключ с нуля</h4>
+                    <hr class="w-full border-t-2 border-white md:border-black"/>
+                    <div class="flex py-5 flex-col">
+                        <div class="flex gap-5">
+                            <span class="text-3xl font-bold text-white md:text-black">01</span>
                             <div>
-                                <p class="mt-3 text-2xl">Разработаем для вас Web-app с любыми пожеданиями под<br> ключ с
-                                    нуля: дизайн, разработка, установка на сервер.
-                                </p>
-                                <ul class="list-decimal pl-6 text-2xl">
-                                    <li>p2e</li>
-                                    <li>Функциональные</li>
-                                    <li>Кликеры</li>
-                                    <li>Магазин</li>
-                                </ul>
+                                <h4 class="text-2xl underline md:no-underline md:text-3xl md:font-bold  text-white md:text-black">
+                                    Покупка готовых Web-app</h4>
+                                <p class="mt-3 text-base md:text-2xl text-white md:text-black">Получите готовый
+                                    кликер,
+                                    интернет-магазин, P2E игру и<br> начните
+                                    путь в мире
+                                    mini-apps</p>
                             </div>
+                        </div>
+                        <div>
+                            <button
+                                class="px-20 md:ml-14 w-full md:w-auto flex justify-center items-center mt-5 border-2 border-white md:border-black text-base md:text-2xl font-bold md:font-normal py-1 md:py-0.5  text-white md:text-black">
+                                приобрести
+                            </button>
+                        </div>
 
-                            <button class="px-20 mt-5 border-2 border-black text-2xl py-0.5 flex">разработать</button>
+                    </div>
+                    <hr class="w-full border-t-2 border-white md:border-black"/>
+                    <div class="flex py-5 flex-col">
+                        <div class="flex gap-5">
+                            <span class="text-3xl font-bold text-white md:text-black">02</span>
+                            <div>
+                                <h4 class="text-2xl underline md:no-underline md:text-3xl md:font-bold  text-white md:text-black">
+                                    Обновление дизайна существующего кликера</h4>
+                                <p class="mt-3 text-base md:text-2xl text-white md:text-black">Модифицируем дизайн для
+                                    вас под любые нужды или изменим кликер под
+                                    ваш дизайн</p>
+
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                class="px-20 md:ml-14 w-full md:w-auto flex justify-center items-center mt-5 border-2 border-white md:border-black text-base md:text-2xl font-bold md:font-normal py-1 md:py-0.5  text-white md:text-black">
+                                приобрести
+                            </button>
+                        </div>
+
+                    </div>
+                    <hr class="w-full border-t-2 border-white md:border-black"/>
+                    <div class="flex py-5 flex-col">
+                        <div class="flex gap-5">
+                            <span class="text-3xl font-bold text-white md:text-black">03</span>
+                            <div>
+                                <h4 class="text-2xl underline md:no-underline md:text-3xl md:font-bold  text-white md:text-black">Разработка Web-app под ключ с нуля</h4>
+                                <div>
+                                    <p class="mt-3 text-base md:text-2xl text-white md:text-black">Разработаем для вас Web-app с любыми пожеданиями под<br>
+                                        ключ с
+                                        нуля: дизайн, разработка, установка на сервер.
+                                    </p>
+                                    <ul class="list-decimal pl-6 text-2xl mt-3 md:text-2xl text-white md:text-black">
+                                        <li>p2e</li>
+                                        <li>Функциональные</li>
+                                        <li>Кликеры</li>
+                                        <li>Магазин</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button
+                                class="px-20 md:ml-14 w-full md:w-auto flex justify-center items-center mt-5 border-2 border-white md:border-black text-base md:text-2xl font-bold md:font-normal py-1 md:py-0.5  text-white md:text-black">
+                                разработать
+                            </button>
                         </div>
                     </div>
-                    <hr class="w-full border-t-2 border-black"/>
+                    <hr class="w-full border-t-2 border-white md:border-black"/>
                 </div>
             </div>
         </div>
